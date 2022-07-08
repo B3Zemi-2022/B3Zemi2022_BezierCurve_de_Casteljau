e656a065536a81c85a2993a967b98fcfb4df3ad7
falnel <20026011@mmm.muroran-it.ac.jp>
2022-07-08 17:55:53 +0900

    修正

--- src/main/java/jp/sagalab/b3semi/BezierCurve.java
+++ src/main/java/jp/sagalab/b3semi/BezierCurve.java
@@ -1,11 +1,15 @@
 package jp.sagalab.b3semi;
 
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
+import java.awt.geom.Line2D;
 
 public final class BezierCurve {
-  public static BezierCurve create(List<Point2D.Double> _controlPoints) {
-    return new BezierCurve(_controlPoints);
+  public static BezierCurve create(List<Point2D.Double> _controlPoints, Canvas _canvas) {
+    return new BezierCurve(_controlPoints, _canvas);
   }
 
   /**
@@ -14,14 +18,60 @@ public final class BezierCurve {
    * @return 評価点
    */
   public Point2D.Double evaluate(double _t) {
-    return new Point2D.Double(/* x, y */);
+    tempPoints.clear();
+    if(m_controlPoints.size() != 1) {
+      for (int i = 0; i < m_controlPoints.size() - 1; ++i) {
+        tempPoints.add(internal(i,_t));
+      }
+      if(tempPoints.size() > 1) drawControl();
+      BezierCurve bezierCurve = BezierCurve.create(tempPoints,m_canvas);
+      Point2D.Double points = bezierCurve.evaluate(_t);
+      return new Point2D.Double(points.getX(), points.getY());
+    }
+    return new Point2D.Double(m_controlPoints.get(0).getX(),m_controlPoints.get(0).getY());
   }
 
   /* ↓ここから必要な関数を書き足していく↓ */
 
-  private BezierCurve(List<Point2D.Double> _controlPoints) {
+  public Point2D.Double internal(int point, double _t){
+    double x = (1 - _t) * m_controlPoints.get(point).getX() + _t * m_controlPoints.get(point + 1).getX();
+    double y = (1 - _t) * m_controlPoints.get(point).getY() + _t * m_controlPoints.get(point + 1).getY();
+    Point2D.Double p = new Point2D.Double(x, y);
+    return p;
+  }
+
+  public void drawControl(){
+    for (Point2D.Double tempPoint : tempPoints)
+      drawPoint(tempPoint.getX(), tempPoint.getY(), Color.green);
+    for(int i=0; i <tempPoints.size()-1;++i)
+      drawLine(tempPoints.get(i),tempPoints.get(i+1),Color.green);
+  }
+
+  public void drawPoint(double _x, double _y, Color c) {
+    Graphics2D g = (Graphics2D)m_canvas.getGraphics();
+    double radius = 6;
+    g.setColor(c);
+    Ellipse2D.Double oval = new Ellipse2D.Double(_x - radius/2, _y - radius/2, radius, radius);
+    g.fill(oval);
+    g.draw(oval);
+  }
+
+  public void drawLine(Point2D.Double _p1, Point2D.Double _p2, Color c) {
+    Graphics2D g = (Graphics2D)m_canvas.getGraphics();
+    g.setColor(c);
+    Stroke BStroke = new BasicStroke(2.0f);
+    g.setStroke(BStroke);
+    Line2D.Double line = new Line2D.Double(_p1, _p2);
+    g.draw(line);
+  }
+
+  private BezierCurve(List<Point2D.Double> _controlPoints, Canvas _canvas) {
     m_controlPoints = _controlPoints;
+    m_canvas = _canvas;
   }
 
   private List<Point2D.Double> m_controlPoints;
+
+  private Canvas m_canvas;
+  List<Point2D.Double> tempPoints = new ArrayList<>();
 }
--- src/main/java/jp/sagalab/b3semi/Main.java
+++ src/main/java/jp/sagalab/b3semi/Main.java
@@ -12,6 +12,9 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yako, takashima
@@ -29,6 +32,7 @@ public class Main extends JFrame {
     add(m_canvas);
     pack();
     setVisible( true );
+    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
 
     m_canvas.addMouseListener(
       new MouseAdapter() {
@@ -39,36 +43,65 @@ public class Main extends JFrame {
 
             // 打った点をリストに追加する
             m_controlPoints.add(p);
-            // 打った点を描画する
-            drawPoint(p.getX(), p.getY());
 
-            // 点が揃ったら
+            // 打った点を描画する
+            drawPoint(p.getX(), p.getY(),Color.BLACK,false);
+
+            //打った点を結んだ線を描画する
+            for(int i = 0; i<m_controlPoints.size()-1; ++i)
+              drawLine(m_controlPoints.get(i),m_controlPoints.get(i+1),Color.lightGray);
+
             if (m_controlPoints.size() == MAX_CONTROL_POINTS) {
-              // ベジェ曲線を描画
-              drawBezierCurve();
+              drawflag = true;
             }
           }
+          if(m_controlPoints.size() == MAX_CONTROL_POINTS && !drawflag) drawflag = true;
         }
       }
     );
+    service.scheduleAtFixedRate(() -> {
+      if(drawflag) {
+        calcBezierCurve(drawcount);
+        drawcount ++;
+        if(drawcount > T){
+          drawflag = false;
+          drawcount = 0;
+        }
+      }
+    }, 0, 100, TimeUnit.MILLISECONDS);
   }
 
-  public void drawBezierCurve() {
-    BezierCurve bezierCurve = BezierCurve.create(m_controlPoints);
 
+  public void calcBezierCurve(double i){
+    canvasClean();
+    for(int j = 0; j<m_controlPoints.size()-1; ++j) drawLine(m_controlPoints.get(j),m_controlPoints.get(j+1),Color.lightGray);
+    for (Point2D.Double m_controlPoint : m_controlPoints) drawPoint(m_controlPoint.getX(), m_controlPoint.getY(), Color.lightGray, false);
+    BezierCurve bezierCurve = BezierCurve.create(m_controlPoints,m_canvas);
     /* ↓ここから必要な処理を書き足していく↓ */
     // コツ: bezierCurve.evaluate(_t) と drawLine(_p1, _p2) を駆使する
+    Point2D.Double p = bezierCurve.evaluate(i/T);
+    BezierPoints.add(p);
+    if(i>0) for(int j=1; j<=i;++j)
+      drawLine(BezierPoints.get(j-1),BezierPoints.get(j),Color.red);
+    drawPoint(BezierPoints.get((int)i).getX(),BezierPoints.get((int)i).getY(),Color.red,true);
   }
 
+  public void canvasClean(){
+    Graphics g = (Graphics)m_canvas.getGraphics();
+    g.setColor(Color.white);
+    g.fillRect(0,0,800,600);
+  }
   /**
    * 点を描画する
    * @param _x x座標
    * @param _y y座標
    */
-  public void drawPoint(double _x, double _y) {
+  public void drawPoint(double _x, double _y, Color c, boolean fill) {
     Graphics2D g = (Graphics2D)m_canvas.getGraphics();
     double radius = 6;
+    g.setColor(c);
     Ellipse2D.Double oval = new Ellipse2D.Double(_x - radius/2, _y - radius/2, radius, radius);
+    if(!fill) g.fill(oval);
     g.draw(oval);
   }
 
@@ -77,8 +110,11 @@ public class Main extends JFrame {
    * @param _p1 始点
    * @param _p2 終点
    */
-  public void drawLine(Point2D.Double _p1, Point2D.Double _p2) {
+  public void drawLine(Point2D.Double _p1, Point2D.Double _p2, Color c) {
     Graphics2D g = (Graphics2D)m_canvas.getGraphics();
+    g.setColor(c);
+    Stroke BStroke = new BasicStroke(2.0f);
+    g.setStroke(BStroke);
     Line2D.Double line = new Line2D.Double(_p1, _p2);
     g.draw(line);
   }
@@ -86,7 +122,7 @@ public class Main extends JFrame {
   /**
    * @param _args the command line arguments
    */
-  public static void main(String[] _args){ new Main(); }
+  public static void main(String[] _args){ new Main();}
 
   /** キャンバスを表す変数 */
   private Canvas m_canvas = new Canvas();
@@ -94,10 +130,18 @@ public class Main extends JFrame {
   /** クリックで打たれた点を保持するリスト */
   private List<Point2D.Double> m_controlPoints = new ArrayList<>();
 
+  /** 求めたベジェ曲線の点を保持するリスト */
+  private List<Point2D.Double> BezierPoints = new ArrayList<>();
+
   /** 点の数の上限
    * （[MAX_CONTROL_POINTS]個の点を打つと、[MAX_CONTROL_POINTS - 1]次のベジェ曲線が描かれる）
    */
-  private static final int MAX_CONTROL_POINTS = 3;
+  private static final int MAX_CONTROL_POINTS = 5;
+  private static final int T = 100;
+
+  private static boolean drawflag = false;
+
+  private static double drawcount = 0;
 
   /**
    * ウィンドウを閉じる時の確認ダイアログを表すクラス
