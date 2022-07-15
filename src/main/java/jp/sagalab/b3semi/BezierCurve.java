1771e04f70a927934cede0037c26e8b1bd699063
unknown <20026059@mmm.muroran-it.ac.jp>
2022-07-15 16:52:45 +0900

    src

--- src/main/java/jp/sagalab/b3semi/BezierCurve.java
+++ src/main/java/jp/sagalab/b3semi/BezierCurve.java
@@ -14,7 +14,20 @@ public final class BezierCurve {
    * @return 評価点
    */
   public Point2D.Double evaluate(double _t) {
-    return new Point2D.Double(/* x, y */);
+    double x;
+    double y;
+    double p_x,p_y;
+    double q_x,q_y;
+
+
+      p_x = m_controlPoints.get(0).x * (1 - _t) + m_controlPoints.get(1).x * _t;
+      p_y = m_controlPoints.get(0).y * (1 - _t) + m_controlPoints.get(1).y * _t;
+      q_x = m_controlPoints.get(1).x * (1 - _t) + m_controlPoints.get(2).x * _t;
+      q_y = m_controlPoints.get(1).y * (1 - _t) + m_controlPoints.get(2).y * _t;
+
+    x = p_x*(1- _t) + q_x*_t;
+    y = p_y*(1- _t) + q_y*_t;
+    return new Point2D.Double(x, y);
   }
 
   /* ↓ここから必要な関数を書き足していく↓ */
